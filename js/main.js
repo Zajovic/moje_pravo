681e2481fae9b4250a6a57fd8a44876589f33341
zajovic <zajovic@hotmail.com>
2020-02-25 22:05:04 +0100

    *

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //  Slick Slider
-$(document).ready(function(){
+$(document).ready(function () {
     $(".carousel-container").slick({
         dots: false,
         arrows: false,
@@ -9,40 +9,55 @@ $(document).ready(function(){
 })
 
 //  Display menu on small screen
-$(document).ready(function(){
-    $(".hamburger-icon-div").click(function(){
-        $(".menu").animate({height:'toggle'},400);
+$(document).ready(function () {
+    $(".hamburger-icon-div").click(function () {
+        $(".menu").animate({ height: 'toggle' }, 400);
     });
-    $(".menu_close-button").click(function(){
-        $(".menu").animate({height:'toggle'},400);
+    $(".menu_close-button").click(function () {
+        $(".menu").animate({ height: 'toggle' }, 400);
     });
 });
 
 //  MODAL
 //  Display modal form
-$(document).ready(function(){
-    $("#mail-aside-icon").click(function() {
-        $(".modal").attr( "style", "display: flex;" );
-        $(".aside-icons").attr( "style", "display: none;" );
+$(document).ready(function () {
+    $("#mail-aside-icon").click(function () {
+        $(".modal").attr("style", "display: flex;");
+        $(".aside-icons").attr("style", "display: none;");
+        console.log("cao")
     });
 });
 
 //  Close modal form  
-$(document).ready(function(){
-    $(".modal_close-button").click(function() {
-        $(".modal").attr( "style", "display: none;");
+$(document).ready(function () {
+    $(".modal_close-button").click(function () {
+        $(".modal").attr("style", "display: none;");
         $(".aside-icons").show();
     });
 });
 
 //   Prevent default on form submit
-$("#mail-form").submit(function(e){
+$("#mail-form").submit(function (e) {
     return false;
 });
 
 //  Tuzbe page article slide down
-$(document).ready(function(){
-    $(".text-article_subtitle-h2--see-more").click(function(){
+$(document).ready(function () {
+    $(".text-article_subtitle-h2--see-more").click(function () {
         $(this).siblings("p").slideDown("1000");
     });
+});
+
+//   Device/OS test
+$(document).ready(function () {
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)) {
+            console.log("Kak da aktivira link");
+    } else {
+        console.log(navigator.userAgent);
+        console.log("ne printa else islov")
+    }
 });
